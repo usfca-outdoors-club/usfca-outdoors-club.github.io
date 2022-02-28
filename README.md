c79416e4b50cd59cbf43859e2fcea8deb626613b
colmpat <cplang@dons.usfca.edu>
2022-02-27 16:46:50 -0800

    first React App commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Club Website for USFCA Outdoors Club
+This is V1 of the Outdoors Club website built by Colm Lang
---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="app">
+      test
+    </div>
+  );
+}
+
+export default App;
